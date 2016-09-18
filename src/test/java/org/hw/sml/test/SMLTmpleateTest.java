ea2ea2a924671721028a1aefd0980dfe66551bc0
wen <huangwenjimmy@163.com>
2016-09-18 16:37:56 +0800

    去除不必要文件

--- src/test/java/org/hw/sml/test/SMLTmpleateTest.java
+++ src/test/java/org/hw/sml/test/SMLTmpleateTest.java
@@ -8,6 +8,7 @@ import org.hw.sml.support.el.JsEl;
 public class SMLTmpleateTest {
 	static String sql="select * from table t where 1=1 <isNotEmpty property=\"a\"> and t.a=#a#</isNotEmpty><isNotEmpty property=\"b\"> and t.b in(#b#)</isNotEmpty>";
 	public static void main(String[] args) {
+		System.out.println(sql);
 		SqlResolvers sqlResolvers=new SqlResolvers(new JsEl());
 		sqlResolvers.init();
 		Rst rst=sqlResolvers.resolverLinks(sql,SMLParams.newSMLParams().add("a","v1").add("b",new String[]{"v2","v3","v4"}).reinit());
