d446b7b8cf8cd75239de87e06a31a2362a0b8435
wen <huangwenjimmy@163.com>
2016-11-30 17:12:49 +0800

    add datasource default implement

---
+++ src/main/java/org/hw/sml/jdbc/impl/DefaultDataSource.java
@@ -0,0 +1,77 @@
+package org.hw.sml.jdbc.impl;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+public class DefaultDataSource implements DataSource{
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	public PrintWriter getLogWriter() throws SQLException {
+		return null;
+	}
+
+	public void setLogWriter(PrintWriter out) throws SQLException {
+	}
+
+	public void setLoginTimeout(int seconds) throws SQLException {
+	}
+	public int getLoginTimeout() throws SQLException {
+		return 0;
+	}
+
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return null;
+	}
+
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return false;
+	}
+	public Connection getConnection() throws SQLException {
+		try {
+			Class.forName(driverClassName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return DriverManager.getConnection(url, username, password);
+	}
+	public Connection getConnection(String username, String password)
+			throws SQLException {
+		return null;
+	}
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException{
+		return null;
+	}
+	
+}
